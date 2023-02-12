b2488cf214457590a95e1b5dd02b66c67f09b174
thomas messett <thomas.messett@stampapp.me>
2023-02-12 20:49:37 +0000

    began setting up vue for iframe

---
+++ publitas-iframe/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>hi</h1>
+</template>
+
+<script>
+const params = window.location.search.slice(1)
+console.log(params)
+
+export default {
+  name: 'App',
+  components: {
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
