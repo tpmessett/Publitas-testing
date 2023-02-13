cd355340fb17b57026d10402a548989214f872e3
thomas messett <thomas.messett@stampapp.me>
2023-02-13 17:57:21 +0000

    basics of iframe structure with vue in place

--- publitas-iframe/src/App.vue
+++ publitas-iframe/src/App.vue
@@ -1,26 +1,70 @@
 <template>
-  <h1>hi</h1>
+  <!-- if params exist get and display cart items -->
+  <div v-if="hasParams">
+    <h1>Your Cart:</h1>
+    <div v-for="product in productList" :key="product.id">
+      <product :product="product"></product>
+    </div>
+  </div>
+  <!-- if no params exist display a simple error -->
+  <div v-else>
+    <div class="empty-cart">
+      <h1>Your Cart Is Empty</h1>
+      <p>Please add some products and try again?</p>
+    </div>
+  </div>
 </template>
 
 <script>
-const params = window.location.search.slice(1)
-console.log(params)
-
-export default {
+import { defineComponent, ref } from 'vue';
+import product from '@/components/product.vue';
+export default defineComponent({
   name: 'App',
   components: {
-
+    product
+  },
+  setup(){
+    // get params which represent cart items, slice off first element as it's a ?
+    const params = window.location.search.slice(1).split(",")
+    // declare a ref as false and if params exist then change it to true
+    const hasParams = ref(false)
+    if (params.length > 0) hasParams.value = true
+    // split params into array of objects so they can be looped easily
+    const productList = []
+    const filterParams = (array, item) => {
+      return array.filter((b) => b == item).length;
+    };
+    params.forEach((param) => {
+      const count = filterParams(params, param)
+      const idArray = productList.map(function (product) {
+        return product.id;
+      });
+      if (idArray.includes(param) === false) {
+        const obj = {ammount: count, id: param}
+        productList.push(obj)
+      }
+    });
+    return {
+      productList,
+      hasParams
+    }
   }
-}
+})
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Avenir Next, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #000;
+}
+.empty-cart {
+  color:  #fff;
+  width: 100%;
+  height: 100vh;
+  padding: 16px;
+  background-color: #b51313;
 }
 </style>
---
+++ publitas-iframe/src/components/product.vue
@@ -0,0 +1,29 @@
+<template>
+  <p>{{product}}</p>
+</template>
+
+<script>
+  import { defineComponent } from 'vue';
+  import { useQuery, useResult } from "@vue/apollo-composable";
+  import { gql } from "@apollo/client/core"
+
+  export default defineComponent({
+    name: 'product',
+    components: {
+
+    },
+    props: {
+      product: Object
+    },
+    setup(props) {
+
+      return {
+
+      }
+    }
+  })
+</script>
+
+<style>
+
+</style>
