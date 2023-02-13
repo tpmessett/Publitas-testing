4df0f26c795121ab5babeac53b562680274dd9f7
thomas messett <thomas.messett@stampapp.me>
2023-02-13 18:40:54 +0000

    got basics of GQL working

--- publitas-iframe/src/components/product.vue
+++ publitas-iframe/src/components/product.vue
@@ -4,7 +4,7 @@
 
 <script>
   import { defineComponent } from 'vue';
-  import { useQuery, useResult } from "@vue/apollo-composable";
+  import { useQuery } from "@vue/apollo-composable";
   import { gql } from "@apollo/client/core"
 
   export default defineComponent({
@@ -16,7 +16,42 @@
       product: Object
     },
     setup(props) {
-
+      const PRODUCT_QUERY = gql`
+        query getProductById {
+          product(handle: "${props.id}") {
+            id
+            title
+            description
+            variants(first: 5) {
+              edges {
+                cursor
+                node {
+                  id
+                  title
+                  quantityAvailable
+                  price {
+                    amount
+                    currencyCode
+                  }
+                  media(first: 1) {
+                    edges {
+                      node {
+                        alt
+                        image {
+                          url
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      `;
+      const { result, error } = useQuery(PRODUCT_QUERY)
+      console.log(result)
+      console.log(error)
       return {
 
       }
