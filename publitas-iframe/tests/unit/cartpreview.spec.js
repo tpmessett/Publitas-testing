f0b3ce5fd40d371075751f57a90b86f438d8fc2a
thomas messett <thomas.messett@stampapp.me>
2023-02-16 00:27:30 +0000

    basic testing outline

---
+++ publitas-iframe/tests/unit/cartpreview.spec.js
@@ -0,0 +1,22 @@
+import {shallowMount} from '@vue/test-utils';
+import cartPreview from '@/components/cartPreview.vue'
+
+describe('cartPreview.vue', () => {
+  // needs me to mock up GQL api response with Jest.mock
+  // it('renders quantity received in props', () => {
+  //   const wrapper = shallowMount(cartPreview, {
+  //     propsData:
+  //     {
+  //       "product": {
+  //         "id": "8109412516122",
+  //         "quantity": 10,
+  //         "variant": "44481714192666"
+  //       }
+  //     }
+  //   });
+  //   expect(wrapper.text()).toContain('10');
+  // })
+  // it('renders correct product details', () => {
+
+  // })
+})
